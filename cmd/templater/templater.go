19c551741ac68940a9d0a502882634c6ef7fa344
Ryan J Yoder <me@ryanjyoder.com>
2019-06-16 21:16:04 -0700

    mvp webserver

--- cmd/templater/templater.go
+++ cmd/templater/templater.go
@@ -3,45 +3,121 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/ryanjyoder/sofp"
 )
 
-var pageTemplate *template.Template
-
 func main() {
 	var err error
-	pageTemplateFilepath := filepath.Join(os.Args[2], "stackoverflow.html")
-	staticAssetsDir := filepath.Join(os.Args[2], "static")
-	pageTemplate, err = template.ParseFiles(pageTemplateFilepath)
-	if err != nil {
-		log.Fatal("failed to load tempalte:", err)
+	templateDir, ok := os.LookupEnv("TEMPLATE_DIR")
+	if !ok {
+		log.Fatal("TEMPLATE_DIR must be set")
 	}
-	http.Handle("/page/assets/", http.StripPrefix("/page/assets/", http.FileServer(http.Dir(staticAssetsDir))))
-	http.HandleFunc("/page/", viewHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	streamsDir, ok := os.LookupEnv("STREAMS_DIR")
+	if !ok {
+		log.Fatal("STREAMS_DIR must be set")
+	}
+
+	listenPort, ok := os.LookupEnv("LISTEN_PORT")
+	if !ok {
+		log.Fatal("LISTEN_PORT must be set")
+	}
+
+	handler, err := NewStackfarHandler(streamsDir, templateDir)
+	if err != nil {
+		log.Fatal("failed to create handler:", err)
+	}
+	http.HandleFunc("/", handler.viewHandler)
+	log.Fatal(http.ListenAndServe(":"+listenPort, nil))
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	p, err := loadPage(filepath.Base(r.URL.Path))
+type StackfarHandler struct {
+	streamsDir   string
+	templateDir  string
+	pageTemplate *template.Template
+}
+
+func NewStackfarHandler(streamsDir string, templateDir string) (*StackfarHandler, error) {
+	pageTemplateFilepath := filepath.Join(templateDir, "stackoverflow.html")
+	pageTemplate, err := template.ParseFiles(pageTemplateFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &StackfarHandler{
+		streamsDir:   streamsDir,
+		templateDir:  templateDir,
+		pageTemplate: pageTemplate,
+	}, nil
+}
+
+func (h *StackfarHandler) viewHandler(w http.ResponseWriter, r *http.Request) {
+	tokens := strings.Split(r.URL.Path, "/")
+	if len(tokens) < 3 {
+		log.Println("url path incorrectly formatted:", r.URL.Path)
+		http.Error(w, "path incorrectly formated", 400)
+		return
+	}
+
+	// assume the domain is redirected: https://askubuntu.com/questions/123
+	domain := strings.Split(r.Host, ":")[0]
+	questionID := tokens[2]
+
+	// if the domain is not redirect to this host the domain will be in the path, eg:  http://localhost/askubuntu.com/questions/123
+	if tokens[1] != "questions" {
+		if len(tokens) < 4 {
+			log.Println("url path incorrectly formatted:", r.URL.Path)
+			http.Error(w, "path incorrectly formated", 400)
+			return
+		}
+		domain = tokens[1]
+		questionID = tokens[3]
+	}
+	if questionID == "assets" {
+		// oops actually an asset no a page
+		h.assetHandler(w, r)
+		return
+
+	}
+	fmt.Println("domain:", domain, "id:", questionID)
+
+	p, err := h.loadPage(domain, questionID)
 	if err != nil {
 		log.Println("error loading page data:", err)
 		http.Error(w, err.Error(), 404)
 		return
 	}
 
-	pageTemplate.Execute(w, p)
+	h.pageTemplate.Execute(w, p)
 }
 
-func loadPage(id string) (*sofp.Question, error) {
-	streamsDir := os.Args[1]
-	paddedFilename := id + "000"
-	filepath := filepath.Join(streamsDir, paddedFilename[:3], id)
+func (h *StackfarHandler) assetHandler(w http.ResponseWriter, r *http.Request) {
+	pathParts := strings.Split(r.URL.Path, "/assets/")
+	if len(pathParts) < 2 {
+		log.Println("url path incorrectly formatted:", r.URL.Path)
+		http.Error(w, "path incorrectly formated", 400)
+		return
+	}
+	path := filepath.Clean(strings.Join(pathParts[1:], "/"))
+	path = filepath.Join(h.templateDir, "static", path)
+	http.ServeFile(w, r, path)
+	return
+}
+
+func (h *StackfarHandler) loadPage(domain string, id string) (*sofp.Question, error) {
+	if len(id) < 1 {
+		return nil, fmt.Errorf("id must not be empty")
+	}
+	subdir := ("000" + id)[len(id):]
+	filepath := filepath.Join(h.streamsDir, domain, subdir, id)
 
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -50,30 +126,25 @@ func loadPage(id string) (*sofp.Question, error) {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	if ok := scanner.Scan(); !ok {
-		log.Fatal("error getting question from stream:", scanner.Err())
-	}
 
-	questionStr := scanner.Text()
-	row := sofp.Row{}
-	json.Unmarshal([]byte(questionStr), &row)
-	question, err := row.GetQuestion()
-	if err != nil {
-		log.Fatal("first row is not a question:", err)
+	question := &sofp.Question{
+		Body: template.HTML("under construction"),
 	}
-
 	for scanner.Scan() {
 		rowStr := scanner.Text()
 		row := sofp.Row{}
 		err = json.Unmarshal([]byte(rowStr), &row)
 		if err != nil {
-			log.Fatal("Error reading stream:", err, rowStr)
+			return nil, err
 		}
-		question.AppendRow(&row)
+		err = question.AppendRow(&row)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	return question, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ryanjyoder/stackfar
 
 go 1.12
 
-require github.com/ryanjyoder/sofp v0.0.0-20190612032325-8110f1afea7f
+require (
+	github.com/golang-commonmark/markdown v0.0.0-20180910011815-a8f139058164
+	github.com/ryanjyoder/sofp v0.0.0-20190617041340-68c092e4f118
+)
